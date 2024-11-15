c9578b6e77935fc379eb7a87f1632783e93557aa
dunyavdl <dunyavanderlinde@gmail.com>
2024-11-15 15:35:22 +0100

    smallangle with docstrings
    smallangle with added docstrings

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -12,8 +12,11 @@ def group():
     "-n",
     "--number",
     default=0,
+    help = "Number is the amount of values between 0 and 2 π to calculate the sine with."
 )
 def sin(number):
+    """Caculate the sine of x, an integer between 0 and 2 π, for n times.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -22,14 +25,16 @@ def sin(number):
 @click.option(
     "-n",
     "--number",
-    default = 0
+    default = 0,
+    help = "Number is the amount of values between 0 and 2 π to calculate the tangent with."
 )
 def tan(number):
+    """Caculate the tangent of x, an integer between 0 and 2 π, for n times.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
 
 if __name__ == "__main__":
-    # sin(10)
     group()
